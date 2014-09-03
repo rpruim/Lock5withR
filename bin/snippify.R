5e709e7ed8d1778e31319e9ce68e8cd1d1bc4b01
Randall Pruim <rpruim@gmail.com>
2014-09-03 16:40:35 -0400

    fix-up snippification process

--- bin/snippify.R
+++ bin/snippify.R
@@ -4,15 +4,18 @@ snippify <- function(file, path, ...) {
   lines <- readLines(f)
   close(f)
 
+  results <- character(0)
   headers <- which(grepl("## ----", lines))
-  start <- head(headers,-1)
-  end <- tail(headers,-1) - 1
+  if (length(headers) < 1) return(results)
+  start <- headers
+  end <- c(tail(headers,-1) - 1, length(lines))
   for (i in 1:length(start)) {
     chunkName <- sub("(## ----)(?<chunk>[^-,]*)(.*)", "\\2", lines[start[i]], 
                      perl = TRUE)
-    if (nchar(chunkName > 0)) {
+    if (is.character(chunkName) && nchar(chunkName > 0) && !grepl("=", chunkName)) {
       chunkPath<- file.path(path, paste0(chunkName,".R"))
       message(paste("  ** Writing", chunkPath))
+      results <- c(results, chunkPath)
       con <- file(chunkPath, "w")
       writeLines( lines[ (start[i] + 1) : end[i] ], con)
       close(con)
@@ -20,3 +23,13 @@ snippify <- function(file, path, ...) {
     }  
   }
 }
+
+require(knitr)
+
+setwd("snipping")
+
+purl("../../Book/Lock5withR.Rnw")
+snippify("Lock5withR.R", path="../../inst/snippets")
+
+setwd("..")
+
