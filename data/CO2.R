fcf21021923844e8882e9b5d5893844838e8cf3e
Randall Pruim <rpruim@gmail.com>
2015-05-18 14:24:20 +1200

    CO2 -> AtmosphericCO2

--- data/CO2.R
+++ data/CO2.R
@@ -1,2 +1,4 @@
-load("CO2.rda")
-names(CO2) <- c("Year", "CO2")  # correcting C02
+# Note this was names CO2 in Lock5Data, but that name conflicts with a data set in datasets.
+load("AtmosphericCO2.rda")
+# used to do this, but now we're saving the rda with the corrected data
+# names(AtmosphericCO2) <- c("Year", "CO2")  # correcting C02
