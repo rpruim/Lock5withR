0c4544c3be5e23816d038aaedb7ce3725778a4d4
Randall Pruim <rpruim@gmail.com>
2015-05-18 15:49:48 +1200

    no longer need to post-process CO2.rda

--- data/CO2.R
+++ data/CO2.R
@@ -1,4 +1,5 @@
 # Note this was names CO2 in Lock5Data, but that name conflicts with a data set in datasets.
-load("AtmosphericCO2.rda")
 # used to do this, but now we're saving the rda with the corrected data
+# file left here as a reminder that CO2 was the original name of the data set in Lock5Data
+# load("AtmosphericCO2.rda")
 # names(AtmosphericCO2) <- c("Year", "CO2")  # correcting C02
