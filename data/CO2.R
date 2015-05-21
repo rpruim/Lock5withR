bf29d3115b8a4f5ceadb3e44e16b6e8010e31cf4
Randall Pruim <rpruim@gmail.com>
2015-05-21 21:31:58 +1200

    Add NA at end

--- data/CO2.R
+++ data/CO2.R
@@ -3,3 +3,4 @@
 # file left here as a reminder that CO2 was the original name of the data set in Lock5Data
 # load("AtmosphericCO2.rda")
 # names(AtmosphericCO2) <- c("Year", "CO2")  # correcting C02
+NULL
